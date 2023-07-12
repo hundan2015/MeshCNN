3ebfcc49474f90fa673a9f9eeee3b73c651f5743
hundan2015 <sym12138@hotmail.com>
2023-07-12 20:07:34 +0800

    new

--- scripts/MSB-obj/test.sh
+++ scripts/MSB-obj/test.sh
@@ -5,7 +5,7 @@ python test.py \
 --dataroot datasets/MSB-obj \
 --name MSB-obj \
 --ncf 64 128 256 256 \
---pool_res 600 450 300 180 \
+--pool_res 740 670 600 570 \
 --norm group \
 --resblocks 1 \
 --export_folder meshes \
