51bc5afae5b9f215147201776fc55eb37491a314
hundan2015 <sym12138@hotmail.com>
2023-07-13 02:23:42 +0800

    sh update

--- scripts/ModelNet9/train.sh
+++ scripts/ModelNet9/train.sh
@@ -5,7 +5,7 @@ python train.py \
 --dataroot datasets/ModelNet9 \
 --name ModelNet9 \
 --ncf 64 128 256 256 \
---pool_res 740 670 600 570 \
+--pool_res 740 670 600 \
 --norm group \
 --resblocks 1 \
 --flip_edges 0.2 \
