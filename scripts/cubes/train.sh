58230afadb62ba3eb7ed869ef85252914bab0251
hundan2015 <sym12138@hotmail.com>
2023-07-14 14:07:02 +0800

    update cubes

--- scripts/cubes/train.sh
+++ scripts/cubes/train.sh
@@ -4,10 +4,12 @@
 python train.py \
 --dataroot datasets/cubes \
 --name cubes \
---ncf 64 128 256 256 \
+--ncf 256 256 256 256 \
 --pool_res 600 450 300 210 \
 --norm group \
 --resblocks 1 \
 --flip_edges 0.2 \
 --slide_verts 0.2 \
 --num_aug 20 \
+--batch_size 64 \
+--num_threads 2 \
