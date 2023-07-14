dc5b53b75f636539e145019a928987881f15f6c6
hundan2015 <sym12138@hotmail.com>
2023-07-14 14:21:24 +0800

    uodate train

--- scripts/cubes/train.sh
+++ scripts/cubes/train.sh
@@ -4,8 +4,8 @@
 python train.py \
 --dataroot datasets/cubes \
 --name cubes \
---ncf 256 256 256 256 \
---pool_res 600 450 300 210 \
+--ncf 256 256 256 \
+--pool_res 600 450 300 \
 --norm group \
 --resblocks 1 \
 --flip_edges 0.2 \
